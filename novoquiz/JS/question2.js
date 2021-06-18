1c43e11d25ac8513b052367dbade509c5e76afd4
Lucas Apalosqui <70145530+lucasApalosqui@users.noreply.github.com>
2021-06-18 15:25:43 -0300

    adicionando página do quiz

---
+++ novoquiz/JS/question2.js
@@ -0,0 +1,38 @@
+
+
+
+const quiz = [
+    {
+    q:'Qual a estimativa de pessoas autistas no Brasil?',
+    options:['500 mil de pessoas','2 milhões de pessoas','1.5 milhoes de pessoas','100 mil pessoas'],
+    answer:1
+},
+{
+    q:'O autismo tem quantos niveis de intensidade?',
+    options:['5 niveis','1 nivel','4 niveis','3 niveis'],
+    answer:3
+},
+{
+    q:'Oque é o autismo?',
+    options:['transtorno de desenvolvimento que prejudica a capacidade de se comunicar e interagir','transtorno de desenvolvimento que prejudica a capacidade de formação de globulos brancos','transtorno mental causado pela má formação do cérebro','agravamento de uma gripe mal curada'],
+    answer:0
+},
+{
+    q:'Quais são os sintomas mais comuns do autismo?',
+    options:['dificuldade de se alimentar, dificuldade de práticar atividades fisicas','queda de cabelo, dificuldade de se movimentar e espasmos involuntários','dificuldade de interação social, dificuldade de comunicação, interesses obsessivos e comportamentos repetitivos','dores de cabeça, dor no corpo e falta de coordenação motora'],
+    answer:2
+},
+
+{
+    q:'O personagem principal da serie de filmes "Harry Potter" tem autismo?',
+    options:['verdadeiro','falso'],
+    answer:1
+},
+
+{
+    q:'Conversas em tons altos incomodam pessoas do espectro autista?',
+    options:['verdadeiro','falso'],
+    answer:0
+},
+
+]
